fe5af3f0c737af3a65236d7dcf19ecd49aa93938
Tim Logtenberg <tim@q42.nl>
2014-11-24 19:10:07 +0100

    username en password uit config

--- Meldpuntongedierte/Meldpuntongedierte/Controllers/LoginController.cs
+++ Meldpuntongedierte/Meldpuntongedierte/Controllers/LoginController.cs
@@ -1,43 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Configuration;
 using System.Web.Mvc;
 using Meldpunt.Services;
-using Meldpunt.Models;
-using Meldpunt.Utils;
 
 namespace Meldpunt.Controllers
 {
-  public class LoginController : Controller
-  {
-    private PageService pageService;
-    private PlaatsService plaatsService;
+	public class LoginController : Controller
+	{
+		private PageService pageService;
+		private PlaatsService plaatsService;
+		private string username = ConfigurationManager.AppSettings["username"];
+		private string password = ConfigurationManager.AppSettings["password"];
 
-    public LoginController()
-    {
-      pageService = new PageService();
-      plaatsService = new PlaatsService();
-    }
+		public LoginController()
+		{
+			pageService = new PageService();
+			plaatsService = new PlaatsService();
+		}
 
-    public ActionResult Login()
-    {
-      return View();
-    }
+		public ActionResult Login()
+		{
+			return View();
+		}
 
-    public ActionResult Logoff()
-    {
-      Session.Clear();
-      return RedirectToAction("Index");
-    }
+		public ActionResult Logoff()
+		{
+			Session.Clear();
+			return RedirectToAction("Index");
+		}
 
-    [HttpPost]
-    public ActionResult Login(string username, string password)
-    {
-      if (username == "admin" && password == "geheim")
-        Session["LoggedIn"] = "loggedIn";
-      return Redirect("/admin");
-    }
+		[HttpPost]
+		public ActionResult Login(string username, string password)
+		{
+			if (username == this.username && password == this.password)
+				Session["LoggedIn"] = "loggedIn";
+			return Redirect("/admin");
+		}
 
-  }
+	}
 }
