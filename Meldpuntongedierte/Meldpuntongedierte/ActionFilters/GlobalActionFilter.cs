b5e88dd921c326ed522f38a1dc3eaf2a640c3881
Tim Logtenberg <tim@q42.nl>
2015-02-28 22:20:10 +0100

    stuff I forgot to commit :|

--- Meldpuntongedierte/Meldpuntongedierte/ActionFilters/GlobalActionFilter.cs
+++ Meldpuntongedierte/Meldpuntongedierte/ActionFilters/GlobalActionFilter.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using System.Configuration;
 using Meldpunt.Services;
 using Meldpunt.Models;
 using Meldpunt.Utils;
