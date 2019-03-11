0596c4904a1374fc26fab6097ebfe788b8ccc135
Tim Logtenberg <tim@q42.nl>
2019-03-11 20:18:22 +0100

    admin styling update

--- Meldpuntongedierte/Meldpuntongedierte/Scripts/admin.js
+++ Meldpuntongedierte/Meldpuntongedierte/Scripts/admin.js
@@ -1,6 +1,19 @@
 $(function () {
-	$(".js-delete-page").submit(function() {
-		if (!confirm("pagina verwijderen?"))
-			return false;
-	});
-})
+  $(".js-delete-page").submit(function () {
+    if (!confirm("pagina verwijderen?"))
+      return false;
+  });
+
+
+  $("[data-action-remove-redirect]").on('click', function (evt) {
+    evt.preventDefault();
+    let id = this.dataset.redirect;
+    console.log(this, id);
+    if (confirm('Verwijderen? Zeker weten?'))
+      window.location = "/admin/removeRedirect?id=" + id;
+    else
+      alert('nou goed dan');
+
+    return false;
+  });
+});
