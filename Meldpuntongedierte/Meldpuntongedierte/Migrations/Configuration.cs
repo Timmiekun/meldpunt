b2c08a4fb17040212ea028cdc604f924074eec83
Tim Logtenberg <tplogtenberg@gmail.com>
2019-06-02 14:40:39 +0200

    removed old gemeente file

--- Meldpuntongedierte/Meldpuntongedierte/Migrations/Configuration.cs
+++ Meldpuntongedierte/Meldpuntongedierte/Migrations/Configuration.cs
@@ -1,14 +1,9 @@
+using System;
+using System.Configuration;
+using System.Data.Entity.Migrations;
+
 namespace Meldpunt.Migrations
 {
-  using Meldpunt.Models;
-  using Meldpunt.Services;
-  using Meldpunt.Utils;
-  using System;
-  using System.Configuration;
-  using System.Data.Entity.Migrations;
-  using System.Linq;
-  using System.Web.Mvc;
-
   internal sealed class Configuration : DbMigrationsConfiguration<Meldpunt.Models.MeldpuntContext>
   {
     public Configuration()
