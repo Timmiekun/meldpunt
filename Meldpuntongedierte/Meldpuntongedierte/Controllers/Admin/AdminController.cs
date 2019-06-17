23253841cd50283294331d41c94a9854cc709229
Tim Logtenberg <tplogtenberg@gmail.com>
2019-06-17 11:02:08 +0200

    Added redirect controller.

--- Meldpuntongedierte/Meldpuntongedierte/Controllers/Admin/AdminController.cs
+++ Meldpuntongedierte/Meldpuntongedierte/Controllers/Admin/AdminController.cs
@@ -1,12 +1,8 @@
 ﻿using Meldpunt.ActionFilters;
 using Meldpunt.Models;
 using Meldpunt.Services;
-using Meldpunt.Services.Interfaces;
-using Meldpunt.Utils;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -16,18 +12,12 @@ namespace Meldpunt.Controllers
   [RoutePrefix("admin")]
   public class AdminController : Controller
   {
-    private IContentPageService pageService;
-    private RedirectService redirectsService;
-    private IImageService imageService;
+   
     private ISearchService searchService;
 
-    public AdminController(IContentPageService _pageService, ISearchService _searchService, IImageService _imageService)
+    public AdminController(ISearchService _searchService)
     {
-      pageService = _pageService;
       searchService = _searchService;
-      redirectsService = new RedirectService();
-      imageService = _imageService;
-
     }
 
     [Route]
@@ -49,59 +39,5 @@ namespace Meldpunt.Controllers
     {
       return View();
     }
-
-
-    #region redirects
-    [Route("Redirects")]
-    public ActionResult Redirects()
-    {
-
-      return View(redirectsService.GetAllRedirects());
-    }
-
-    [HttpPost]
-    [Route("Redirects")]
-    public ActionResult Redirects(RedirectModel redirect)
-    {
-      if (!ModelState.IsValid)
-        return View(redirectsService.GetAllRedirects());
-
-      var existingRoute = RouteTable.Routes
-        .OfType<Route>()
-        .Select(r => (Route)r)
-        .FirstOrDefault(r => r.Url == redirect.From.TrimStart('/'));
-
-      if (existingRoute != null)
-      {
-        ModelState.AddModelError("alreadyExists", "Deze url wordt gebruikt door een bestaande pagina");
-        return View(redirectsService.GetAllRedirects());
-      }
-
-      var existingRedirect = redirectsService.FindByFrom(redirect.From);
-      if (existingRedirect != null && existingRedirect.Id != redirect.Id)
-      {
-        ModelState.AddModelError("alreadyExists", "Er bestaat al een redirect van deze url (" + redirect.From + ")");
-        return View(redirectsService.GetAllRedirects());
-      }
-
-      redirectsService.SaveRedirect(redirect);
-      return View(redirectsService.GetAllRedirects());
-    }
-
-    [Route("NewRedirect")]
-    public ActionResult NewRedirect(string parentId)
-    {
-      var redirect = redirectsService.newRedirect();
-      return Redirect("/admin/redirects#" + redirect.Id);
-    }
-
-    [Route("RemoveRedirect")]
-    public ActionResult RemoveRedirect(string id)
-    {
-      redirectsService.deleteRedirect(id);
-      return RedirectToAction("Redirects");
-    }
-    #endregion
-
   }
 }
---
+++ Meldpuntongedierte/Meldpuntongedierte/Controllers/Admin/AdminRedirectController.cs
@@ -0,0 +1,83 @@
+﻿using Meldpunt.ActionFilters;
+using Meldpunt.Models;
+using Meldpunt.Services;
+using Meldpunt.Services.Interfaces;
+using Meldpunt.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Meldpunt.Controllers
+{
+  [MustBeAdmin]
+  [RoutePrefix("admin")]
+  public class AdminRedirectController : Controller
+  {
+
+    private RedirectService redirectsService;
+    private ISearchService searchService;
+
+    public AdminRedirectController(IContentPageService _pageService, ISearchService _searchService, IImageService _imageService)
+    {
+      searchService = _searchService;
+      redirectsService = new RedirectService();
+    }
+
+    [Route("Redirects")]
+    public ActionResult Redirects()
+    {
+
+      return View(redirectsService.GetAllRedirects());
+    }
+
+    [HttpPost]
+    [Route("Redirects")]
+    public ActionResult Redirects(RedirectModel redirect)
+    {
+      if (!ModelState.IsValid)
+        return View(redirectsService.GetAllRedirects());
+
+      var existingRoute = RouteTable.Routes
+        .OfType<Route>()
+        .Select(r => (Route)r)
+        .FirstOrDefault(r => r.Url == redirect.From.TrimStart('/'));
+
+      if (existingRoute != null)
+      {
+        ModelState.AddModelError("alreadyExists", "Deze url wordt gebruikt door een bestaande pagina");
+        return View(redirectsService.GetAllRedirects());
+      }
+
+      var existingRedirect = redirectsService.FindByFrom(redirect.From);
+      if (existingRedirect != null && existingRedirect.Id != redirect.Id)
+      {
+        ModelState.AddModelError("alreadyExists", "Er bestaat al een redirect van deze url (" + redirect.From + ")");
+        return View(redirectsService.GetAllRedirects());
+      }
+
+      redirectsService.SaveRedirect(redirect);
+
+      Response.RemoveOutputCacheItem(redirect.From);
+
+      return View(redirectsService.GetAllRedirects());
+    }
+
+    [Route("NewRedirect")]
+    public ActionResult NewRedirect(string parentId)
+    {
+      redirectsService.newRedirect();
+      return Redirect("/admin/redirect");
+    }
+
+    [Route("RemoveRedirect")]
+    public ActionResult RemoveRedirect(string id)
+    {
+      redirectsService.deleteRedirect(id);
+      return RedirectToAction("Redirects");
+    }
+
+  }
+}
